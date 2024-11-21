0edcc1254374e3c9a61e928f529b9eb064241098
Fabio Carneiro <fabioaacarneiro@gmail.com>
2024-11-20 23:38:09 -0300

    feat: adiciona model para tabela de reset de senha

---
+++ app/Models/PasswordResetModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordResetModel extends Model
+{
+    use HasFactory;
+    
+    public $timestamps = false;
+    
+    protected $table = 'password_reset_token';
+
+    protected $fillable = [
+        'email',
+        'token',
+        'created_at',
+    ];
+}
