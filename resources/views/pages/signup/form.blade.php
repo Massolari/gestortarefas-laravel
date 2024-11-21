fe7c273fdcfec01be584102574545f881f1d2892
Fabio Carneiro <fabioaacarneiro@gmail.com>
2024-11-21 19:32:48 -0300

    refactor: ajusta erros dos formulários

--- resources/views/pages/signup/form.blade.php
+++ resources/views/pages/signup/form.blade.php
@@ -7,21 +7,46 @@
             <h3 class="card-title text-info">Finalize seu cadastro</h3>
             <form class="form-group d-flex flex-column gap-2" action="{{ route('signup.submit') }}" method="POST">
                 @csrf
-                <input type="hidden" name="email" value="{{ $email }}">
+                <input type="hidden" name="email" value="{{ session('email') }}">
                 
                 <div class="form-floating">
-                    <input type="text" name="name" required class="form-control">
+                    <input 
+                        type="text" 
+                        name="name" 
+                        required 
+                        class="form-control"
+                        value="{{ old('name') }}"
+                    >
                     <label for="name">Nome:</label>
+                    @error('name')
+                        <p class="text-danger text-center">{{ $message }}</p>
+                    @enderror
                 </div>
             
                 <div class="form-floating">
-                    <input type="password" name="password" required class="form-control">
+                    <input 
+                        type="password" 
+                        name="password" 
+                        required 
+                        class="form-control"
+                    >
                     <label for="password">Senha:</label>
+                    @error('password')
+                        <p class="text-danger text-center">{{ $message }}</p>
+                    @enderror
                 </div>
             
                 <div class="form-floating">
-                    <input type="password" name="password_confirm" required class="form-control">
+                    <input 
+                        type="password" 
+                        name="password_confirm" 
+                        required 
+                        class="form-control"
+                    >
                     <label for="password_confirm">Confirme a senha:</label>
+                    @error('password_confirm')
+                        <p class="text-danger text-center">{{ $message }}</p>
+                    @enderror
                 </div>
     
                 <button type="submit" class="btn btn-primary">Cadastrar</button>
